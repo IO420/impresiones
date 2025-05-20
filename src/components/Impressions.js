387b41574c0d64758b9c55dc8dd82635b16a9921
IO420 <320154041@pcpuma.acatlan.unam.mx>
2025-05-19 21:18:38 -0400

    build

--- src/components/Impressions.js
+++ src/components/Impressions.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
 import axios from 'axios';
+import { useState } from 'react';
 import { envConfig } from '../envConfigurations/EnvConfigurations';
+import { useNavigate } from 'react-router-dom';
 import './login.css';
 
 export const Impressions = () => {
@@ -10,37 +11,45 @@ export const Impressions = () => {
     const [folio, setFolio] = useState('');
     const [monto, setMonto] = useState('');
     const [fecha, setFecha] = useState('');
-    const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
+    const [alert, setAlert] = useState('');
     const [studentData, setStudentData] = useState(null);
 
     const url = envConfig().apiUrl;
     const token = localStorage.getItem('token');
+    const navigate = useNavigate();
 
     const headers = {
         Authorization: `Bearer ${token}`,
     };
 
     const handleBuscar = async () => {
+        setAlert('')
+        setError('')
         try {
-            const response = await axios.get(`${url}/student`, {
-                headers,
-                params: {
+            const response = await axios.post(
+                `${url}/student`,
+                {
                     numAccount: numero,
                 },
-            });
+                {
+                    headers,
+                }
+            );
             const data = response.data;
             setStudentData(data);
-            setMessage('Estudiante encontrado');
+            setAlert('Estudiante encontrado');
         } catch (error) {
-            console.error('Error al buscar:', error);
-            setMessage('No se encontró el estudiante');
+            setError(error.response?.data?.error || 'No se encontró el estudiante');
             setStudentData(null);
         }
     };
 
     const handleCobrar = async () => {
+        setAlert('')
+        setError('')
         if (!studentData) {
-            setMessage('Primero busca un estudiante');
+            setError('Error busca denuevo al estudiante');
             return;
         }
         try {
@@ -53,16 +62,18 @@ export const Impressions = () => {
                 },
                 { headers }
             );
-            setMessage('Cobro realizado correctamente');
+            setAlert('Cobro realizado correctamente');
+            handleBuscar()
         } catch (error) {
-            console.error(error);
-            setMessage('Error al realizar el cobro');
+            setError(error.response?.data?.error || 'Error al realizar el cobro');
         }
     };
 
     const handleGuardarRecibo = async () => {
+        setAlert('')
+        setError('')
         if (!studentData) {
-            setMessage('Primero busca un estudiante');
+            setError('Primero busca un estudiante');
             return;
         }
         try {
@@ -76,11 +87,16 @@ export const Impressions = () => {
                 },
                 { headers }
             );
-            setMessage('Recibo guardado correctamente');
+            setAlert('Recibo guardado correctamente');
+            handleBuscar()
         } catch (error) {
-            console.error(error);
-            setMessage('Error al guardar el recibo');
+            setError(error.response?.data?.error || 'Error al guardar el recibo');
         }
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/Login');
     };
 
     return (
@@ -101,103 +117,113 @@ export const Impressions = () => {
                         </button>
                     </div>
 
-                    <div className="information">
-                        <label>No.Cuenta:</label>
-                        <label>Nombre:</label>
-                        <label>Credito:</label>
-                    </div>
+                    {studentData && (
+                        <div className="information">
+                            <label>No.Cuenta: {studentData[0]?.id_cuenta}</label>
+                            <label>Nombre: {studentData[0]?.nombre}</label>
+                            <label>Crédito: {studentData[0]?.credito}</label>
+                        </div>
+                    )}
                 </div>
             </div>
 
-            <div className="selection">
-                <div className="toggle-group">
-                    <button
-                        className={`toggle-button ${view === 'impresiones' ? 'active' : ''}`}
-                        onClick={() => setView('impresiones')}
-                    >
-                        Impresiones
-                    </button>
-                    <button
-                        className={`toggle-button ${view === 'recibo' ? 'active' : ''}`}
-                        onClick={() => setView('recibo')}
-                    >
-                        Recibo
-                    </button>
+            {studentData &&
+                <div className="selection">
+                    <div className="toggle-group">
+                        <button
+                            className={`toggle-button ${view === 'impresiones' ? 'active' : ''}`}
+                            onClick={() => setView('impresiones')}
+                        >
+                            Impresiones
+                        </button>
+                        <button
+                            className={`toggle-button ${view === 'recibo' ? 'active' : ''}`}
+                            onClick={() => setView('recibo')}
+                        >
+                            Recibo
+                        </button>
+                    </div>
+
+
+                    {view === 'impresiones' && (
+                        <div className="impressions">
+                            <div className="groupLabel">
+                                <label className="input-label">Costo: $1.00</label>
+                            </div>
+
+                            <div className="input-group">
+                                <label className="input-label">Hojas:</label>
+                                <input
+                                    type="text"
+                                    value={hojas}
+                                    onChange={(e) => setHojas(e.target.value)}
+                                    className="input-field "
+                                    placeholder='Numero de hojas a imprimir...'
+                                />
+                            </div>
+
+                            <div className="groupLabel">
+                                <label className="input-label">Total: {hojas && `$${hojas}.00`}</label>
+                            </div>
+
+                            <button onClick={handleCobrar} className="button button-charge">
+                                Cobrar
+                            </button>
+                        </div>
+                    )}
+
+                    {view === 'recibo' && (
+                        <div className="impressions">
+                            <div className="input-group">
+                                <label className="input-label">Folio:</label>
+                                <input
+                                    type="text"
+                                    value={folio}
+                                    onChange={(e) => setFolio(e.target.value)}
+                                    className="input-field"
+                                    placeholder='Numero de folio...'
+                                />
+                            </div>
+
+                            <div className="input-group">
+                                <label className="input-label">Monto:</label>
+                                <input
+                                    type="text"
+                                    value={monto}
+                                    onChange={(e) => setMonto(e.target.value)}
+                                    className="input-field"
+                                    placeholder='Monto recibido...'
+                                />
+                            </div>
+
+                            <div className="input-group">
+                                <label className="input-label">Fecha:</label>
+                                <input
+                                    type="date"
+                                    value={fecha}
+                                    onChange={(e) => setFecha(e.target.value)}
+                                    className="input-field"
+                                />
+                            </div>
+
+                            <button onClick={handleGuardarRecibo} className="button button-search">
+                                Guardar
+                            </button>
+                        </div>
+                    )}
                 </div>
-
-
-                {view === 'impresiones' && (
-                    <div className="impressions">
-                        <div className="groupLabel">
-                            <label className="input-label">Costo: $1.00</label>
-                        </div>
-
-                        <div className="input-group">
-                            <label className="input-label">Hojas:</label>
-                            <input
-                                type="text"
-                                value={hojas}
-                                onChange={(e) => setHojas(e.target.value)}
-                                className="input-field "
-                                placeholder='Numero de hojas a imprimir...'
-                            />
-                        </div>
-
-                        <div className="groupLabel">
-                            <label className="input-label">Total: {hojas && `$${hojas}.00`}</label>
-                        </div>
-
-                        <button onClick={handleCobrar} className="button button-charge">
-                            Cobrar
-                        </button>
-                    </div>
-                )}
-
-                {view === 'recibo' && (
-                    <div className="impressions">
-                        <div className="input-group">
-                            <label className="input-label">Folio:</label>
-                            <input
-                                type="text"
-                                value={folio}
-                                onChange={(e) => setFolio(e.target.value)}
-                                className="input-field"
-                                placeholder='Numero de folio...'
-                            />
-                        </div>
-
-                        <div className="input-group">
-                            <label className="input-label">Monto:</label>
-                            <input
-                                type="text"
-                                value={monto}
-                                onChange={(e) => setMonto(e.target.value)}
-                                className="input-field"
-                                placeholder='Monto recibido...'
-                            />
-                        </div>
-
-                        <div className="input-group">
-                            <label className="input-label">Fecha:</label>
-                            <input
-                                type="date"
-                                value={fecha}
-                                onChange={(e) => setFecha(e.target.value)}
-                                className="input-field"
-                            />
-                        </div>
-
-                        <button onClick={handleGuardarRecibo} className="button button-search">
-                            Guardar
-                        </button>
-                    </div>
-                )}
-            </div>
-            {message &&
-                <div
-                    className={`messageBox ${message.includes('exitoso') ? 'success' : 'error'}`}
-                >
-                    {message}
+            }
+            <button onClick={handleLogout} className="button button-logout">
+                Cerrar sesión
+            </button>
+            {error &&
+                <div className={`messageBox error`}>
+                    {error}
+                </div>
+            }
+            {alert &&
+                <div className={`messageBox success`}>
+                    {alert}
                 </div>
             }
         </>
